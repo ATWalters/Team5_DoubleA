aa52967cabea7db5066ac3300ddf7c8bc2c1ec23
ATWalters <austintwalters1998@gmail.com>
2021-04-15 08:48:37 -0400

    The Map now gets the user's location and zooms in on that spot

--- app/src/main/java/cpsc4150/allaroundclemson/MapsActivity.java
+++ app/src/main/java/cpsc4150/allaroundclemson/MapsActivity.java
@@ -1,19 +1,40 @@
 package cpsc4150.allaroundclemson;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private final int REQUEST_LOCATION_PERMISSIONS = 0;
+
     private GoogleMap mMap;
+
+    private FusedLocationProviderClient mClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private float mZoomLevel = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +44,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Creating location request
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(3000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        //Create location callback
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                if(locationResult != null){
+                    for(Location location : locationResult.getLocations()){
+                        updateMap(location);
+                    }
+                }
+            }
+        };
+
+        mClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
+    private void updateMap(Location location){
+        //Get user's current location
+        LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        //Place a marker at the current location
+        MarkerOptions myMarker = new MarkerOptions()
+                .title("You are here!")
+                .position(myLatLng);
+
+        //Remove previous marker
+        mMap.clear();
+
+        //Add new marker
+        mMap.addMarker(myMarker);
+
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(myLatLng, mZoomLevel);
+        mMap.moveCamera(update);
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -38,9 +98,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Used in making sure the GoogleMap was working correctly before going ahead with
+        //adding all of our functionality
+
+        // Add a marker in Clemson and move the camera
+        //LatLng clemson = new LatLng(34.6793, -82.8351);
+        //mMap.addMarker(new MarkerOptions().position(clemson).title("Marker in Clemson"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(clemson, 19));
+
+        mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener(){
+            @Override
+            public void onCameraMove(){
+                CameraPosition cameraPosition = mMap.getCameraPosition();
+                mZoomLevel = cameraPosition.zoom;
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (hasLocationPermission()) {
+            mClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        }
+    }
+
+    private boolean hasLocationPermission() {
+
+        // Request fine location permission if not already granted
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this ,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    REQUEST_LOCATION_PERMISSIONS);
+
+            return false;
+        }
+
+        return true;
     }
 }
